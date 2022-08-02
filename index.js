1f4c96965356c4db795bb5deef8c739101432f6a
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2022-08-02 08:42:25 +0200

    added route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (request, response) => {
+  response.send("Hi");
 });
